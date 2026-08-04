4160614006e67b25329ac95dabe64e5198943932
Louiz Alfredo Dela Cruz <louiz_delacruz@dlsu.edu.ph>
2026-08-04 18:10:10 +0800

    Update SPConversion.js
    fixed handling of special cases

--- SPConversion.js
+++ SPConversion.js
@@ -75,8 +75,34 @@ function handleConversion() {
         }
 
         let E = exp + 101; //exponent
-        if (E < 0 || E > 191) {
-            alert("Exponent out of bounds");
+        if (E > 191) {
+            if (signBit === "1") {
+                binaryResult = "1 11110 000000 00000000000000000000";
+                hexResult = "0xF8000000";
+                specialCase = "-Infinity (Overflow)";
+            } else {
+                binaryResult = "0 11110 000000 00000000000000000000";
+                hexResult = "0x78000000";
+                specialCase = "+Infinity (Overflow)";
+            }
+            document.getElementById("out-convert-binary").innerText = binaryResult;
+            document.getElementById("out-convert-hex").innerText = hexResult;
+            document.getElementById("out-convert-special").innerText = specialCase;
+            return;
+        } 
+        else if (E < 0) {
+            if (signBit === "1") {
+                binaryResult = "1 01000 100101 00000000000000000000";
+                hexResult = "0xA2A00000";
+                specialCase = "-Zero (Underflow)";
+            } else {
+                binaryResult = "0 01000 100101 00000000000000000000";
+                hexResult = "0x22A00000";
+                specialCase = "+Zero (Underflow)";
+            }
+            document.getElementById("out-convert-binary").innerText = binaryResult;
+            document.getElementById("out-convert-hex").innerText = hexResult;
+            document.getElementById("out-convert-special").innerText = specialCase;
             return;
         }
 
